93467b32965e5d4854fcd6be0fff7d8bd3abfd83
isha-mistry <ishamistry.lamprostech@gmail.com>
2023-12-10 02:40:53 +0530

    Created payout contract and added contract ABI and integrated Disperse function

--- src/components/dashboard/ScholarshipApplicants.js
+++ src/components/dashboard/ScholarshipApplicants.js
@@ -1,15 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/dashboard/ScholarshipApplicants.css";
+import { contractInstance } from "../Contract";
+import { useAccount } from "wagmi";
+import { useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
 
 function ScholarshipApplicants() {
+  const navigate = useNavigate();
+  const [amount, setAmount] = useState();
+
+  const executePayout = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        if (!provider) {
+          console.log("Metamask is not installed, please install!");
+        }
+        const con = await contractInstance();
+        console.log("Amount", amount);
+
+        const recipientsArray = [
+          "0x6e2ae46509297aB25344F50504D5Ae50fBBB4292",
+          "0x5719413E4Dc736Dd60D73cb8B5734F19a96535ae",
+        ];
+
+        const amountInWei = ethers.utils.parseEther(amount);
+
+        console.log("Amount in Wei: ", amountInWei, {
+          value: amount * recipientsArray.length,
+        });
+
+        const tx = await con.disperse(recipientsArray, amountInWei.toString(), {
+          value: (amountInWei * recipientsArray.length).toString(),
+        });
+        console.log();
+
+        console.log(tx);
+        await tx.wait();
+      }
+    } catch (e) {
+      console.log("Error in creating user account: ", e);
+    }
+  };
+
   return (
     <div className="py-3 col-10 mx-auto">
       <div className="d-flex py-3 applicants-components my-4">
-        <div className="d-flex align-items-center ps-4 col-8 applicant-component-title">
+        <div className="d-flex align-items-center ps-4 col-7 applicant-component-title">
           Scholarship Name
         </div>
-        <div className="col-4">
-          <button type="button" className="btn btn-primary">
+        <div className="col-5">
+          <input
+            type="text"
+            className="mx-2"
+            value={amount}
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+          ></input>
+          <button
+            type="button"
+            className="btn btn-primary"
+            style={{ fontSize: "1.2rem", fontWeight: "600" }}
+            onClick={executePayout}
+          >
             Execute Payout
           </button>
         </div>
